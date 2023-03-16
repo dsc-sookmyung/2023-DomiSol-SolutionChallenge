7d9a21347fa0bac5d11601b31ee9db32e6244156
yejin <yaezzin@gmail.com>
2023-03-16 16:56:03 +0900

    Chore : List

--- src/main/java/domisol/domain/board/controller/BoardController.java
+++ src/main/java/domisol/domain/board/controller/BoardController.java
@@ -4,12 +4,14 @@ import domisol.domain.board.dto.VoiceResponse;
 import domisol.global.BaseResponse;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/boards")
 public class BoardController {
 
     @GetMapping("/test")
-    public BaseResponse<VoiceResponse> getJson(@RequestBody VoiceResponse response) {
+    public BaseResponse<List<VoiceResponse>> getJson(@RequestBody List<VoiceResponse> response) {
         return new BaseResponse<>(response);
     }
 
